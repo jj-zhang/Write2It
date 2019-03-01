6858cdfc6b0632f06d68777e65a3eb7886699d42
chrchung <cachungchristina@gmail.com>
2019-03-01 09:37:42 -0500

    Modified the html structure to adhere to bootstrap's guidelines

--- writeit/src/CreateStory/CreateStory.js
+++ writeit/src/CreateStory/CreateStory.js
@@ -4,28 +4,33 @@ import { create } from 'domain';
 class createStory extends React.Component {
     render(){
         return (
-            <div class="container col-lg-6 col-xs-12">
+            <div class="createstory page">
+                <div class="container">
+                    <div class="row">
+                        <div class=" col-lg-6 col-xs-12">
+                            <form class="ui form">
+                                <div class="field">
+                                    <h2><strong>Title</strong></h2>
+                                    <div class="ui input">
+                                        <input type="text" name="first-name" placeholder="Title"/>
+                                    </div>
+                                </div>
 
-                <form class="ui form">
-                <div class="field">
-                    <h2><strong>Title</strong></h2>
-                    <div class="ui input">
-                        <input type="text" name="first-name" placeholder="Title"/>
+                                <div class="field">
+                                    <h2><strong>Opening Sentence</strong></h2>
+                                    <h4>Your sentence must include the word <strong class="teal">Frog</strong>.</h4>
+
+                                    <textarea placeholder="Write something..." required></textarea>
+                                </div>
+
+                                <button class="ui teal button" type="submit">Create Story</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-
-                <div class="field">
-                    <h2><strong>Opening Sentence</strong></h2>
-                    <h4>Your sentence must include the word <strong class="teal">Frog</strong>.</h4>
-
-                    <textarea placeholder="Write something..." required></textarea>
-                </div>
-
-
-                <button class="ui teal button" type="submit">Create Story</button>
-
-                </form>
             </div>
+
+
         );
     }
 }
