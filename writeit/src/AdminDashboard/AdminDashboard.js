0c25a3ce1751db748a46770ac03bdb66a5ae7332
junjie zhang <darren.zhang@mail.utoronto.ca>
2019-03-04 01:31:00 -0500

    Finished the overall report

--- writeit/src/AdminDashboard/AdminDashboard.js
+++ writeit/src/AdminDashboard/AdminDashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import UserStatus from './UserStatus';
+import Reports from './Reports';
+
 
 class AdminDashboard extends Component {
     render() {
@@ -12,8 +14,32 @@ class AdminDashboard extends Component {
                 </div>
                 <div className="ui segment">
                     <div className="userList ui middle aligned divided list">
+                        <div className="item">
+                                    <div className="right floated content">
+                                        <div className="ui small red button">Remove</div>
+                                    </div>
+                                    <div className="content">
+                                        <div className="header">Snickerdoodle</div>
+                                    </div>
+                        </div>
                     </div>
                 </div>
+
+
+
+
+                <div class="ui segment userReports">
+                    <h2>New Reports</h2>
+                    <div class="ui middle aligned divided list">
+                    <Reports/>
+                    </div>
+
+                    <h2>Archived Reports</h2>
+                    <div class="ui middle aligned divided list">
+                    </div>
+                </div>
+
+
             </div>
         );
     }
