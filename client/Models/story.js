0ad655a9d272e20dcf14e232e605f0beb3688682
junjie zhang <darren.zhang@mail.utoronto.ca>
2019-03-22 01:41:59 -0400

    Add User model to MongoDB.

---
+++ client/Models/story.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const story = new mongoose.Schema({
+    title: String,
+    description: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "userid"
+        },
+        userName: String,
+        password: String,
+        userType: {
+            type: String,
+            enum: ['admin','user']
+        },
+        email: String,
+        // Note sure about this type
+        dateCreated: Date
+    },
+     dateCreated: {
+        type: Date, 
+        default: Date.now
+    },
+    upvotes: [{
+        type: int,
+        ref: "upvote"
+    }],
+    sentences: [{
+        type: String,
+        ref: "sentence"
+    }]
+ });
+
+ // Export the model
+ module.exports = mongoose.model("story", story);
