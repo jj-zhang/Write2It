48929b57c471720e4ba9f9c55958c61413f89788
chrchung <cachungchristina@gmail.com>
2019-03-22 18:51:45 -0400

    User schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,43 @@
-/**
- * Created by christina on 2019-03-22.
- */
+'use strict';
+
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Not valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    sentences: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
+
+
+
+
