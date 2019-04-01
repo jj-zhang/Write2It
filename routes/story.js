71de8e2ea94b4f5b4cb065a657a33e6bec880c3b
chrchung <cachungchristina@gmail.com>
2019-04-01 07:35:40 -0400

    Just included a new property to store total upvote count (so we dont have to compute it everytime we go to landing page)

--- routes/story.js
+++ routes/story.js
@@ -1,57 +1,82 @@
+'use strict';
+
 const mongoose = require('mongoose');
-const { ObjectID } = require('mongodb')
-const { Story } = require('../models/story');
+var mongoosePaginate = require('mongoose-paginate');
+const {ObjectID} = require('mongodb')
+const {Story} = require('../models/story');
+
+
 module.exports = function (app) {
 
 
     app.post('/story',
-        (req, res)=>{
-            console.log(req.body);
-            var connection = mongoose.createConnection('mongodb://localhost:27017/WriteItAPI');
-            const story = connection.model("story");
-            story.create(req.body, 
-                (error,story)=>{
-                    if (error){
-                        res.status(400).send(error);
-                    }else{
-                        res.send(story)
-                    }
-                    connection.close();
-                })
+        (req, res) => {
+
+            const story = new Story({
+                title: req.body.title,
+                description: req.body.description,
+                user: req.body.userID
+            });
+
+            // save story
+            story.save().then((result) => {
+                res.send(result);
+            }, (error) => {
+                res.status(400).send(error);
+            });
+
+
         }
-    )
+    );
 
 
     app.get('/story',
-        (req,res)=>{
-            var connection = mongoose.createConnection('mongodb://localhost:27017/WriteItAPI');
-            const story = connection.model("story");
-            story.find(req.body.condition).exec(
-                (error, document)=>{
-                    if (error){
-                        res.status(400).send(error);
-                    }else{
-                        res.send(document);
+        (req, res) => {
+            Story.find().then((result) => {
+                res.send(result);
+            }, (error) => {
+                res.status(500).send(error);
+            });
+        });
+
+
+    // get a page of stories
+    app.get('/story/:page',
+        (req, res) => {
+            Story.paginate({}, {page: req.params.page, limit: 5})
+                .then((result) => {
+                    if (result) {
+                        res.send(result);
+                    } else {
+                        res.status(404).send();
                     }
-                    connection.close();
-                }
-            )
-        })
 
-
-    app.post('/upvote/:storyId',(req,res) => {
-        const storyId = req.params.storyId;
-	    if (!ObjectID.isValid(storyId)) {
-		    res.status(404).send()
-	    };
-        Story.findById(storyId).then((story) => {
-            if(!story){
-                res.status(404).send()
-            }else{
-                story.upvotes.push({
-                    vote:req.body.vote,
-                    user:req.body.userID
+                })
+                .catch((error) => {
+                    res.status(500).send(error);
                 });
+    });
+
+
+    app.post('/upvote/:storyId', (req, res) => {
+        const storyId = req.params.storyId;
+        if (!ObjectID.isValid(storyId)) {
+            res.status(404).send()
+        }
+
+        Story.findById(storyId).then((story) => {
+            if (!story) {
+                res.status(404).send()
+            } else {
+
+                story.upvoteCount += req.body.vote;
+
+                story.upvotes.push({
+                    vote: req.body.vote,
+                    user: req.body.userID
+                });
+
+
                 story.save().then((result) => {
                     res.send(result);
                 }, (error) => {
@@ -59,43 +84,48 @@ module.exports = function (app) {
                 })
             }
         })
-    })
-    app.post('/removeUpvote/:storyId',(req,res) => {
+    });
+
+    app.post('/removeUpvote/:storyId', (req, res) => {
         const storyId = req.params.storyId;
-	    if (!ObjectID.isValid(storyId)) {
-		    res.status(404).send()
-	    };
+        if (!ObjectID.isValid(storyId)) {
+            res.status(404).send()
+        };
+
         Story.findById(storyId).then((story) => {
-            if(!story){
+            if (!story) {
                 res.status(404).send()
-            }else{
-                story.update({ $pull : { upvotes : { user: req.body.userID}}}, function(err, result) {
+            } else {
+                story.update({$pull: {upvotes: {user: req.body.userID}}}, function (err, result) {
                     if (err) {
                         res.status(400).send(err);
-                    }else{
+                    } else {
                         res.send(result);
                     }
                 });
             }
         })
-    })
-    app.post('/upvote/:storyId/:sentenceId',(req,res) => {
+    });
+
+    app.post('/upvote/:storyId/:sentenceId', (req, res) => {
         const storyId = req.params.storyId;
         const sentenceId = req.params.sentenceId;
-	    if (!ObjectID.isValid(storyId)) {
-		    res.status(404).send()
-        };
+        if (!ObjectID.isValid(storyId)) {
+            res.status(404).send()
+        }
+        ;
         if (!ObjectID.isValid(sentenceId)) {
-		    res.status(404).send()
-	    };
+            res.status(404).send()
+        }
+        ;
         Story.findById(storyId).then((story) => {
-            if(!story){
+            if (!story) {
                 res.status(404).send()
-            }else{
+            } else {
                 var sentence = story.sentences.id(sentenceId);
                 sentence.upvotes.push({
-                    vote:req.body.vote,
-                    user:req.body.userID
+                    vote: req.body.vote,
+                    user: req.body.userID
                 });
                 story.save().then((result) => {
                     res.send(result);
@@ -104,26 +134,27 @@ module.exports = function (app) {
                 })
             }
         })
-    })
-    app.post('/removeUpvote/:storyId/:sentenceId',(req,res) => {
+    });
+
+    app.post('/removeUpvote/:storyId/:sentenceId', (req, res) => {
         const storyId = req.params.storyId;
         const sentenceId = req.params.sentenceId;
-	    if (!ObjectID.isValid(storyId)) {
-		    res.status(404).send()
-        };
+        if (!ObjectID.isValid(storyId)) {
+            res.status(404).send()
+        }
         if (!ObjectID.isValid(sentenceId)) {
-		    res.status(404).send()
-	    };
+            res.status(404).send()
+        }
         Story.findById(storyId).then((story) => {
-            if(!story){
+            if (!story) {
                 res.status(404).send()
-            }else{
+            } else {
                 story.updateOne(
-                    {"sentences._id" : sentenceId},
-                    {"$pull": { "sentences.upvotes" : {"user":req.body.userID}}}
-                )
+                    {"sentences._id": sentenceId},
+                    {"$pull": {"sentences.upvotes": {"user": req.body.userID}}}
+                );
                 res.send(story);
             }
         })
     })
-}
+};
