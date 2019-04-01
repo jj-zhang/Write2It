9eb2c4ded4f49596826cc2e3784e1b62e967f5e7
chrchung <cachungchristina@gmail.com>
2019-04-01 11:27:49 -0400

    delete upvote for sentence (not yet tested)

--- routes/story.js
+++ routes/story.js
@@ -105,7 +105,7 @@ module.exports = function (app) {
             }
             ,
             $inc: {
-                upvoteCount: 1
+                upvoteCount: -1
             }
         }, {new: true}).then((result) => {
 
@@ -120,26 +120,28 @@ module.exports = function (app) {
     });
 
 
-    app.post('/removeUpvote/:storyId', (req, res) => {
-        const storyId = req.params.storyId;
-        if (!ObjectID.isValid(storyId)) {
-            res.status(404).send()
-        };
+    // above api call properly implements it as a delete request
 
-        Story.findById(storyId).then((story) => {
-            if (!story) {
-                res.status(404).send()
-            } else {
-                story.update({$pull: {upvotes: {user: req.body.userID}}}, function (err, result) {
-                    if (err) {
-                        res.status(400).send(err);
-                    } else {
-                        res.send(result);
-                    }
-                });
-            }
-        });
-    });
+    // app.post('/removeUpvote/:storyId', (req, res) => {
+    //     const storyId = req.params.storyId;
+    //     if (!ObjectID.isValid(storyId)) {
+    //         res.status(404).send()
+    //     };
+    //
+    //     Story.findById(storyId).then((story) => {
+    //         if (!story) {
+    //             res.status(404).send()
+    //         } else {
+    //             story.update({$pull: {upvotes: {user: req.body.userID}}}, function (err, result) {
+    //                 if (err) {
+    //                     res.status(400).send(err);
+    //                 } else {
+    //                     res.send(result);
+    //                 }
+    //             });
+    //         }
+    //     });
+    // });
 
 
     app.post('/upvote/:storyId/:sentenceId', (req, res) => {
@@ -148,16 +150,19 @@ module.exports = function (app) {
         if (!ObjectID.isValid(storyId)) {
             res.status(404).send()
         }
-        ;
+
         if (!ObjectID.isValid(sentenceId)) {
             res.status(404).send()
         }
-        ;
+
         Story.findById(storyId).then((story) => {
             if (!story) {
                 res.status(404).send()
             } else {
                 var sentence = story.sentences.id(sentenceId);
+
+                sentence.upvoteCount += 1;
+
                 sentence.upvotes.push({
                     vote: req.body.vote,
                     user: req.body.userID
@@ -172,48 +177,65 @@ module.exports = function (app) {
     });
 
 
-    app.delete('/upvote/:storyId/:sentenceId', (req, res) => {
+    app.delete('/upvote/:storyId/:sentenceId/:upvoteId', (req, res) => {
         const storyId = req.params.storyId;
         const sentenceId = req.params.sentenceId;
-        if (!ObjectID.isValid(storyId)) {
+        const upvoteId = req.params.upvoteId;
+
+        if (!ObjectID.isValid(storyId) || !ObjectID.isValid(sentenceId) || !ObjectID.isValid(upvoteId)) {
             res.status(404).send()
         }
-        if (!ObjectID.isValid(sentenceId)) {
-            res.status(404).send()
-        }
-        Story.findById(storyId).then((story) => {
-            if (!story) {
-                res.status(404).send()
-            } else {
-                story.updateOne(
-                    {"sentences._id": sentenceId},
-                    {"$pull": {"sentences.upvotes": {"user": req.body.userID}}}
-                );
-                res.send(story);
+
+        Story.updateOne({
+            '_id': storyId,
+            'sentences._id': sentenceId,
+            'sentences.upvotes._id': upvoteId
+        }, {
+            $pull: {
+                'sentences.upvotes': {
+                    _id: upvoteId
+                }
             }
-        })
-    });
+            ,
+            $inc: {
+                'sentences.upvoteCount': -1
+            }
+        }, {new: true}).then((result) => {
+
+            if (!result) {
+                res.status(404).send(error);
+            }
+
+            res.send(result);
+        }, (error) => {
+            res.status(400).send(error);
+        });
 
 
-    app.post('/removeUpvote/:storyId/:sentenceId', (req, res) => {
-        const storyId = req.params.storyId;
-        const sentenceId = req.params.sentenceId;
-        if (!ObjectID.isValid(storyId)) {
-            res.status(404).send()
-        }
-        if (!ObjectID.isValid(sentenceId)) {
-            res.status(404).send()
-        }
-        Story.findById(storyId).then((story) => {
-            if (!story) {
-                res.status(404).send()
-            } else {
-                story.updateOne(
-                    {"sentences._id": sentenceId},
-                    {"$pull": {"sentences.upvotes": {"user": req.body.userID}}}
-                );
-                res.send(story);
-            }
-        })
+
     });
+
+    // above api call properly implements it as a delete request
+
+    // app.post('/removeUpvote/:storyId/:sentenceId', (req, res) => {
+    //     const storyId = req.params.storyId;
+    //     const sentenceId = req.params.sentenceId;
+    //     if (!ObjectID.isValid(storyId)) {
+    //         res.status(404).send()
+    //     }
+    //     if (!ObjectID.isValid(sentenceId)) {
+    //         res.status(404).send()
+    //     }
+    //     Story.findById(storyId).then((story) => {
+    //         if (!story) {
+    //             res.status(404).send()
+    //         } else {
+    //             story.updateOne(
+    //                 {"sentences._id": sentenceId},
+    //                 {"$pull": {"sentences.upvotes": {"user": req.body.userID}}}
+    //             );
+    //             res.send(story);
+    //         }
+    //     })
+    // });
 };
