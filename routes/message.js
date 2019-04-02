f89f6353bf7e0d1ec8be34623a289b1825f3daa8
chrchung <cachungchristina@gmail.com>
2019-04-02 10:11:39 -0400

    route for updating messa

--- routes/message.js
+++ routes/message.js
@@ -4,37 +4,56 @@ const {Message} = require("../models/messages");
 const {authenticateAdmin, authenticateUser} = require("./authentication");
 module.exports = function (app) {
 
-app.post('/message', (req, res) => {
-    console.log(req.session.user);
-    const message = new Message({
-        message: req.body.message,
-        sender: req.session.user? req.session.user:null
+    app.post('/message', (req, res) => {
+        console.log(req.session.user);
+        const message = new Message({
+            message: req.body.message,
+            sender: req.session.user ? req.session.user : null
+        });
+
+        // save user to the database
+        message.save().then((message) => {
+            res.send({error: false});
+        }, (error) => {
+            res.status(400).send();
+        })
     });
 
-    // save user to the database
-    message.save().then((message) => {
-        res.send({error: false});
-    }, (error) => {
-        res.status(400).send();
-    })
-});
+    app.patch('/message/:id', (req, res) => {
+        const id = req.params.id;
 
-app.get('/message/:id',(req, res) => {
-    const id = req.params.id;
-
-    if (!ObjectID.isValid(id)) {
-		res.status(404).send();
-    }
-    
-    Message.findById(id).then(message => {
-        if (!message) {
+        if (!ObjectID.isValid(id)) {
             res.status(404).send();
-        } else {
-            res.send({message});
         }
-    }).catch((error) => {
-        res.status(500).send()
-    });
-});
 
-}
+        Restaurant.findByIdAndUpdate(id, {
+        }, {$set: req.body}, {new: true}).then((result) => {
+            if(!result) {
+                res.status(404).send();
+            } else {
+                res.send(result);
+            }
+        }).catch((error) => {
+            res.status(400).send(error);
+        });
+    });
+
+    app.get('/message/:id', (req, res) => {
+        const id = req.params.id;
+
+        if (!ObjectID.isValid(id)) {
+            res.status(404).send();
+        }
+
+        Message.findById(id).then(message => {
+            if (!message) {
+                res.status(404).send();
+            } else {
+                res.send({message});
+            }
+        }).catch((error) => {
+            res.status(500).send()
+        });
+    });
+
+};
