15c8e3d34eb793096a59fda49498f5e4d52298b7
chrchung <cachungchristina@gmail.com>
2019-04-02 10:15:37 -0400

    add timestamp

--- models/messages.js
+++ models/messages.js
@@ -4,16 +4,17 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
     message: String,
-    sender: {type:mongoose.Schema.Types.ObjectId, required:false, ref:"user"},
+    sender: {type: mongoose.Schema.Types.ObjectId, required: false, ref: "user"},
     solved: {
         type: mongoose.Schema.Types.Boolean,
         default: false
-    }
+    },
+    timestamps: true
 });
 
 const Message = mongoose.model('message', MessageSchema);
 
-module.exports = { Message };
+module.exports = {Message};
 
 
 
